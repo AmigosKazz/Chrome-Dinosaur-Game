bc70bd8d845ed41bc5c875ee902d58f00d7e9088
AmigosKazz <kaznarahandrinarivo@gmail.com>
2024-12-17 22:07:42 +0300

    initialize this project and configure the environment.

---
+++ README.md
@@ -0,0 +1,4 @@
+_Fill bord in life so I just made this game to make me feel better. This game make in java using a java swing library._
+
+
+_Copyright (c) 2024 by [Amigoskazz](https://amigoskazz.me/)_
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int boardWidth = 750;
+        int boardHeight = 250;
+
+        JFrame frame = new JFrame("chrome Dinosaur game");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
